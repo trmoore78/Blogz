206010cfff744e8b462973ab205b232e14f3474f
Torrie Moore <trmoore78@yahoo.com>
2019-04-24 23:08:45 -0500

    updating files

--- main.py
+++ main.py
@@ -3,11 +3,21 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://get-it-done:beproductive@localhost:8889/get-it-done'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:beproductive@localhost:8889/build-a-blog'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
+class Blog(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(120))
+    body = db.Column(db.String(120))
+    completed = db.Column(db.Boolean)
 
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
+        self.completed = False
+        
 class Task(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
