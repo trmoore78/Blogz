4cfc880c3f233ea2a904a65cc972d1af9fdb921e
Torrie Moore <trmoore78@yahoo.com>
2019-05-15 18:46:43 -0500

    updated code

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-from flask import Flask, request, redirect, render_template,url_for
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 import cgi
 
+
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:hello1@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:hello19@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
+app.secret_key = 'y337kGcys&ZP38'
 
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True)
+    password = db.Column(db.String(25))
+    blogs = db.relationship('Blog',backref='owner')
+
+    def __init__(self, username, password, blogs, owner):
+        self.username = username
+        self.password = password
+        self.blogs = blogs
+        self.owner = owner
 
 class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(120))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
     def __init__(self, title, body):
         self.title = title
         self.body = body
+    
         
 
 @app.route('/blog',methods=['GET'])
